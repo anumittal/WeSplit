1699fcbff9e9ab188c9e02238edf3ac56e0a7026
Anu Mittal <anu22mittal@gmail.com>
2021-02-01 21:14:00 +0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anu Mittal on 01/02/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
